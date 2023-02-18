38bfe904365817169b648b20054e69a22c88ac03
owenmatejka <owenmatejka+github@gmail.com>
2023-02-18 14:22:55 -0800

    Update class.lua
    Added timeout settings and function

--- src/server/GameSafe Server/Modules/Player/class.lua
+++ src/server/GameSafe Server/Modules/Player/class.lua
@@ -8,7 +8,17 @@ function PlayerClass.new(player)
     self.Player = player
     self.PlayerID = player.UserId
 
+    self.Data = {
+        AllowedMinutes = 10;
+        CurrentMinutes = 20;
+        PauseState = "Idle";
+    }
+
     return self
+end
+
+function PlayerClass:HasExceededTime()
+    return self.Data.AllowedMinutes <= self.Data.CurrentMinutes
 end
 
 return PlayerClass
