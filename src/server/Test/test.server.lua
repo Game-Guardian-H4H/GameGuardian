0cfc47a4d11544cce04a5ac0676aa051f9e53288
owenmatejka <owenmatejka+github@gmail.com>
2023-02-18 15:50:41 -0800

    Update test.server.lua
    Test overriding

--- src/server/Test/test.server.lua
+++ src/server/Test/test.server.lua
@@ -7,3 +7,9 @@ local GameSafe = game:GetService("ServerScriptService").Server["GameSafe Server"
 local Runner = require(GameSafe.runner)
 Runner.Init()
 Runner:Start()
+
+local HookManager = require(game:GetService("ReplicatedStorage").Common["GameSafe Shared"].Hooks)
+
+HookManager:HookEvent("PauseEvent", function(player)
+    player:Kick("Example Override")
+end)
