9835683e664377998ada00d7fa81476a98b7bdfc
owenmatejka <owenmatejka+github@gmail.com>
2023-02-18 17:54:57 -0800

    Update default.lua
    Created remote to connect with client

--- src/shared/GameSafe Shared/Hooks/default.lua
+++ src/shared/GameSafe Shared/Hooks/default.lua
@@ -1,7 +1,15 @@
 local DefaultHook = {}
 
-DefaultHook["PauseEvent"] = function(player)
-    player:Kick("Pause Event")
+local RemotesFolder = Instance.new("Folder")
+RemotesFolder.Name = "Remotes"
+RemotesFolder.Parent = script.Parent.Parent
+
+local PauseRemote = Instance.new("RemoteEvent")
+PauseRemote.Name = "Pause Remote"
+PauseRemote.Parent = RemotesFolder
+
+DefaultHook["PauseEvent"] = function(player, state)
+    PauseRemote:FireClient(player, state == "Enabled")
 end
 
 return DefaultHook
